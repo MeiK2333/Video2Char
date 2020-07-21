fc4176af2a4bbd015a4ce50e3ab79c575b435dea
MeiK <meik2333@gmail.com>
2020-07-21 11:46:09 +0800

    基本完成 Video 部分

--- js/v2c.js
+++ js/v2c.js
@@ -1,5 +1,5 @@
 class V2C {
-  constructor(width, height, charset) {
+  constructor(width, height, charset, grayImageCanvas = null, colorfulImageCanvas = null) {
     this.charset = charset;
     this.width = width;
     this.height = height;
@@ -9,13 +9,13 @@ class V2C {
     this.ctx = this.canvas.getContext('2d');
     this.ctx.font = "10px monospace";
 
-    this.imageCanvas = document.createElement('canvas');
-    this.imageCanvas.width = this.width * 7;
-    this.imageCanvas.height = this.height * 7;
-    this.imageCtx = this.imageCanvas.getContext('2d');
-    this.imageCtx.font = "10px monospace";
+    this.grayImageCanvas = grayImageCanvas || document.createElement('canvas');
+    this.grayImageCanvas.width = this.width * 7;
+    this.grayImageCanvas.height = this.height * 7;
+    this.grayImageCtx = this.grayImageCanvas.getContext('2d');
+    this.grayImageCtx.font = "10px monospace";
 
-    this.colorfulImageCanvas = document.createElement('canvas');
+    this.colorfulImageCanvas = colorfulImageCanvas || document.createElement('canvas');
     this.colorfulImageCanvas.width = this.width * 7;
     this.colorfulImageCanvas.height = this.height * 7;
     this.colorfulImageCtx = this.colorfulImageCanvas.getContext('2d');
@@ -24,8 +24,8 @@ class V2C {
 }
 
 class V2CVideo extends V2C {
-  constructor(videoElem, width, height, charset) {
-    super(width, height, charset);
+  constructor(videoElem, width, height, charset, grayImageCanvas = null, colorfulImageCanvas = null) {
+    super(width, height, charset, grayImageCanvas, colorfulImageCanvas);
     this.videoElem = videoElem;
   }
   screenshot(gray = false, colorful = false) {
@@ -33,10 +33,14 @@ class V2CVideo extends V2C {
     this.ctx.drawImage(this.videoElem, 0, 0, this.canvas.width, this.canvas.height);
     const image = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
     if (gray) {
-      this.imageCtx.clearRect(0, 0, this.imageCanvas.width, this.imageCanvas.height);
+      this.grayImageCtx.clearRect(0, 0, this.grayImageCanvas.width, this.grayImageCanvas.height);
+      // this.grayImageCtx.fillStyle = '#fff';
+      // this.grayImageCtx.fillRect(0, 0, this.grayImageCanvas.width, this.grayImageCanvas.height);
     }
     if (colorful) {
       this.colorfulImageCtx.clearRect(0, 0, this.colorfulImageCanvas.width, this.colorfulImageCanvas.height);
+      // this.colorfulImageCtx.fillStyle = '#fff';
+      // this.colorfulImageCtx.fillRect(0, 0, this.colorfulImageCanvas.width, this.colorfulImageCanvas.height);
     }
     const frame = [];
     let pos = 0;
@@ -52,8 +56,8 @@ class V2CVideo extends V2C {
         line.push(char);
 
         if (gray) {
-          this.imageCtx.fillStyle = `red`;
-          this.imageCtx.fillText(char, j * 7, 7 * i + 7);
+          this.grayImageCtx.fillStyle = `rgba(0, 0, 0, 1)`;
+          this.grayImageCtx.fillText(char, j * 7, 7 * i + 7);
         }
         if (colorful) {
           this.colorfulImageCtx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
@@ -63,27 +67,26 @@ class V2CVideo extends V2C {
       frame.push(line.join(''));
     }
     const text = frame.join('\n');
-    let grayImage = null;
-    if (gray) {
-      grayImage = this.imageCtx.getImageData(0, 0, this.imageCanvas.width, this.imageCanvas.height);
-    }
-    let colorfulImage = null;
-    if (colorful) {
-      colorfulImage = this.colorfulImageCtx.getImageData(0, 0, this.colorfulImageCanvas.width, this.colorfulImageCanvas.height);
-    }
     return {
       text: text,
-      grayImage: grayImage,
-      colorfulImage: colorfulImage
+      grayImage: this.grayImageCanvas,
+      colorfulImage: this.colorfulImageCanvas
     }
   }
 }
 
-function downloadGray(grayImages) {
-  // TODO: 生成 GIF 并触发下载
-  for (let i = 0; i < grayImages.length; i++) {
-    const image = grayImages[i];
+let ALPHA_THRESHOLD = 128;
+
+function downloadGIF(images) {
+  const encoder = new GIFEncoder();
+  encoder.setRepeat(0);
+  encoder.setDelay(100);
+  encoder.start();
+  for (const image of images) {
+    encoder.addFrame(image, true);
   }
+  encoder.finish();
+  encoder.download("output.gif");
 }
 
 function downloadText(texts) {
